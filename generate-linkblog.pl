b83157f52a10d7b24b7ac74781ca150cd11ec227
kevinspencer <kevin@kevinspencer.org>
2022-09-15 16:19:11 -0700

    adding date parsing logic

--- generate-linkblog.pl
+++ generate-linkblog.pl
@@ -12,6 +12,9 @@
 ################################################################################
  
 use Data::Dumper;
+use DateTime;
+use File::Copy;
+use Time::Seconds;
 use WWW::Pinboard;
 use utf8::all;
 use strict;
@@ -19,9 +22,10 @@ use warnings;
 
 $Data::Dumper::Indent = 1;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
-my $token = $ENV{PINBOARD_TOKEN} or die "No pinboard API token found\n";
+my $token    = $ENV{PINBOARD_TOKEN} or die "No pinboard API token found\n";
+my $finaldir = $ENV{LINKBLOG_DIR} or die "No linkblog directory found\n";
 
 my $pinboard = WWW::Pinboard->new(token => $token);
 
@@ -55,20 +59,62 @@ close($htfh);
 
 print $lbfh "<ul class=\"wppb-bookmarks\">\n";
 
+my $dt1 = DateTime->now(time_zone => 'America/Phoenix');
+
 for my $post (@{$list->{posts}}) {
+
+    my $dt2;
+    if ($post->{time} =~ /^(\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)Z$/) {
+        $dt2 = DateTime->new(year => $1, month => $2, day => $3, hour => $4, minute => $5, second => $6, time_zone  => 'GMT');
+        $dt2->set_time_zone("America/Phoenix")
+    }
+
+    my $time_ago_string = format_time_ago_string($dt1->epoch(), $dt2->epoch());
+
     print $lbfh "    <li class=\"wppb-bookmark\">\n";
     print $lbfh "        <div class=\"wppb-header\"><a class=\"wppb-title\" href=\"$post->{href}\"><i class=\"fa fa-star\"></i> $post->{description}</a></div>\n";
     print $lbfh "        <div class=\"wppb-description\">$post->{extended}</div>\n";
     print $lbfh "        <div class=\"wppb-footer\">\n";
     print $lbfh "            <i class=\"fa fa-clock-o\" aria-hidden=\"true\"></i>\n";
-    print $lbfh "            <abbr class=\"wppb-date\" title=\"$post->{time}\">6 days ago</abbr>\n";
+    print $lbfh "            <abbr class=\"wppb-date\" title=\"$post->{time}\">$time_ago_string</abbr>\n";
     print $lbfh "        </div>\n";
     print $lbfh "    </li>\n";
 }
 
 print $lbfh "</ul>\n";
 
-print $lbfh $_ while(<$ftfh>);
+my $now = lc(localtime());
+
+while (<$ftfh>) {
+    my $line = $_;
+    if ($line =~ /RUNTIME/) {
+        $line =~ s/RUNTIME/$now/;
+    }
+    print $lbfh $line;
+}
 
 close($lbfh);
 close($ftfh);
+
+sub format_time_ago_string {
+    my ($timestamp_now, $timestamp_then) = @_;
+
+    my $duration = ($timestamp_now - $timestamp_then);
+
+    my $duration_string;
+    if ( $duration > ONE_YEAR ) {
+        $duration_string = sprintf "%d years ago", $duration / ONE_YEAR;
+        return $duration_string;
+    } elsif ($duration > ONE_DAY) {
+        $duration_string = sprintf "%d days ago", $duration / ONE_DAY;
+        return $duration_string;
+    } elsif ($duration > ONE_HOUR) {
+        $duration_string = sprintf "%d hours ago", $duration / ONE_HOUR;
+        return $duration_string;
+    } else {
+        $duration_string = sprintf "%d mins ago", $duration / ONE_MINUTE;
+        return $duration_string;
+    }
+}
+
+
